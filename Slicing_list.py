d53ed2f1541b94d2e58b9912b5d99f869be53e10
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2026-05-10 23:10:59 +0600

    added new files

---
+++ Slicing_list.py
@@ -0,0 +1,9 @@
+# sonkha = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# name_list = ["Akash", "Arafat", "Mito"]
+# mixed_list = [40, 405, 30, "Noman", 123.34, True]
+# empty_list = []
+# print(name_list[10])
+
+# name = ["Akash", "Arafat", "Mito", "Dola", "Sadia", "Rimi", "Sashank"]
+# print(name[:])
+
--- loops.py
+++ loops.py
@@ -6,7 +6,101 @@
 #     print(chr)
 
 
-fruits = ['apple','banana','mango']
-for x in fruits:
-    print(fruits[x])
+# fruits = ['apple','banana','mango']
+# for x in fruits:
+#     print(fruits[x])
 
+# for x in range(1,10,2):
+#     print(x)
+# for x in range(0,-5,-1):
+#     print(x)
+
+# kichuekta_number = range(100)
+# print(kichuekta_number)
+# print(list(kichuekta_number))
+
+# count = 1
+
+# while count <= 10:
+#     print(count)
+#     count = count+1
+
+# print(count)
+
+# for i in range(1,11):
+#     if i==5:
+#         break
+#     print(i)
+
+# count = 0
+# while True:
+#     if count == 5:
+#         break
+#     # count += 1
+#     count = count + 1
+#     print(count)
+
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(i)
+
+# for i in range(1, 10):
+#     pass
+
+# for i in range(1,11):
+#     for j in range(1,16):
+#         print(f"i={i} j={j}")
+
+# row = 5
+# for i in range(1, 6):
+#     print("*" * i)
+
+
+# for i in range(6,1,1):
+#     print("*"*i)
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,10):
+#     print(i)
+# else:
+#     print("Loop break!!")
+
+# for i in range(1,11):
+#     if i==5:
+#         break
+#     print(i)
+# else:
+#     print("Loop break!!")
+
+# students = ["a", "b", "c", "d"]
+# target = "a"
+# for student in students:
+#     if student == target:
+#         print("Found!!!!")
+#         break
+# else:
+#     print("Not Found!!!!")
+
+# students = ["a", "b", "c", "d"]
+
+# for index, value in enumerate(students):
+#     print(index, value)
+
+
+students = ["a", "b", "c", "d"]
+# marks = [12, 11, 10]
+# for name, mark in zip(students, marks):
+#     print(name,"=", mark)
+
+for i in reversed(students):
+    print(i)
